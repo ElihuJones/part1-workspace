f7fdc6904a7b977caef0b539576df66cbabe7e1f
ElihuJones <xelijone@amazon.com>
2022-09-06 13:56:36 -0400

    Added verifyInternetConnection

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,13 +1,41 @@
 public class Television {
 
-  String brand; // brand name
-  int volume;    // current volume
+  private String brand; // brand name
 
-  void turnOn () {
+  private int volume;   // current volume
+
+  // Model/Business Methods --> public methods
+  public void turnOn () {
+    boolean isConnected = verifyInternetConnection();
     System.out.println("Turning on your " + brand + " TV to volume " + volume);
   }
 
-  void turnOff() {
+  public void turnOff() {
     System.out.println("Shutting down " + brand + " TV, goodbye ");
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public int getVolume() {
+    return volume;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  //Helper Methods --> private methods
+  private boolean verifyInternetConnection() {
+    return true;
+  }
+
+  public String toString() {
+    return "Television: brand = " + brand + ", volume = " + volume;
   }
 }
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -3,18 +3,20 @@ public class TelevisionClient {
   public static void main(String[] args) {
 
     Television tv1 = new Television();
-    tv1.brand = "Sony";
-    tv1.volume = 25;
+    tv1.setBrand("Sony");
+    tv1.setVolume(25);
 
     tv1.turnOn();
     tv1.turnOff();
         
     Television tv2 = new Television();
-    tv2.brand = "Samsung";
-    tv2.volume = 32;
+    tv2.setBrand("Samsung");
+    tv2.setVolume(32);
 
     tv2.turnOn();
     tv2.turnOff();
 
+    System.out.println(tv1.toString()); //These two lines are identical
+    System.out.println(tv1);
   }
 }
