cf55df7fe4d0b8d4181a5fe3e71afa8e1ee836c5
ElihuJones <xelijone@amazon.com>
2022-09-16 15:46:46 -0400

    Completed Lab 11.1

--- Lab11.1/src/com/hr/personnel/client/HRClient.java
+++ Lab11.1/src/com/hr/personnel/client/HRClient.java
@@ -12,7 +12,6 @@ import com.hr.personnel.Department;
 import com.hr.personnel.Employee;
 import com.hr.personnel.HourlyEmployee;
 import com.hr.personnel.SalariedEmployee;
-import gov.irs.IllegalWageException;
 import java.time.LocalDate;
 
 /**
@@ -26,16 +25,13 @@ class HRClient {
         System.out.println(dept);
 
         // add Employees to it
-        try {
-            dept.addEmployee(new SalariedEmployee("Jason",  LocalDate.of(1990, 8, 24), 2250.0));
-            dept.addEmployee(new HourlyEmployee("Julie",    LocalDate.of(2000, 2, 2), 35.0, 30.0));
-            dept.addEmployee(new SalariedEmployee("Amilia", LocalDate.of(1999, 2, 6), 1250.0));
-            dept.addEmployee(new HourlyEmployee("Logan",    LocalDate.of(2002, 6, 2), 40.0, 40.0));
-        } catch (IllegalWageException e) {
-            System.out.println(" ERROR: " + e);         //this sout has a string concatenation with an object. The toString() method is invoked
-                                                        // can also use souf ("ERROR: %s%n" , e);
-                                                        // sout and souf yields the same thing.
-        }
+        dept.addEmployee(new SalariedEmployee("Jason", LocalDate.of(1990, 8, 24), 2500)); //will have to change these two line
+        dept.addEmployee(new HourlyEmployee("Julie", LocalDate.of(2000, 2, 2), 20.90, 40));
+
+
+        //todo create at least 1 HourlyEmployee and 1 SalariedEmployee passing to the addEmployee method. page 110
+        dept.addEmployee(new SalariedEmployee("Elihu", LocalDate.of(2022, 8, 29), 4000));
+        dept.addEmployee(new HourlyEmployee("Jurgen", LocalDate.of(2020, 1, 10), 27.50, 40));
 
         // list its Employees
         System.out.println("\nList employees:");
@@ -45,8 +41,9 @@ class HRClient {
         System.out.println("\nMake employees work:");
         dept.workEmployees();
 
-        // pay its Employees
-        System.out.println("\nPay all employees:");
+        //Pay its employees
+        System.out.println("\nPay employees:");
         dept.payEmployees();
     }
+
 }
